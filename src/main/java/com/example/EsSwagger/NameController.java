dffcc9714ef7447315b0fc5a84b0c7815b33e6ad
SIlvioVutea <silviovutea@gmail.com>
2024-05-26 11:53:13 +0200

    done

---
+++ src/main/java/com/example/EsSwagger/NameController.java
@@ -0,0 +1,22 @@
+package com.example.EsSwagger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1")
+public class NameController {
+
+    @Autowired
+    Name name;
+
+    @GetMapping(path = "/name")
+    public String getName(@RequestParam String name) {
+        return this.name.get(name);
+    }
+
+    @PostMapping("/name/reversedName")
+    public String reverseName(@RequestParam String name) {
+        return this.name.reverse(name);
+    }
+}
---
+++ src/test/java/com/example/EsSwagger/EsSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EsSwagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsSwaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
